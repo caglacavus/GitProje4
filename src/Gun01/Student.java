42dd8c401b02e1b56d6e0dc562c76dd1ee1abbfb
caglacavus <caglaacavus@gmail.com>
2023-01-26 19:29:37 +0300

    task 2 yapıldı(taskın adı)

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,6 +4,8 @@ public class Student {
     public static void main(String[] args) {
 
 
-        System.out.println("merhaba 1");
+        System.out.println("Merhaba 1");
+
+        System.out.println("task 2 yapıldı");
     }
 }
